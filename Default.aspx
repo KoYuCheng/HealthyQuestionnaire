c7182fdd11fa13139be30d8785eda28ebac79ef0
KoYuCheng <108333876+KoYuCheng@users.noreply.github.com>
2022-07-07 14:37:30 +0800

    Add files via upload

---
+++ Default.aspx
@@ -0,0 +1,110 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+    <style>
+        .shadowbox {
+            width: 15em;
+            border: 1px solid #333;
+            box-shadow: 8px 8px 5px #444;
+            padding: 8px 12px;
+            background-image: linear-gradient(180deg, #fff, #ddd 40%, #ccc);
+        }
+    </style>
+</head>
+<body>
+   
+    <form id="form1" runat="server">
+       
+    
+        <h1>健康生活問卷</h1>
+       <fieldset><legend></legend>
+           
+        姓名:<asp:TextBox ID="name" runat="server" OnTextChanged="name_TextChanged"></asp:TextBox>
+
+        <br />性別:<asp:RadioButtonList ID="gender" runat="server" RepeatDirection="Horizontal" RepeatLayout="Flow" OnSelectedIndexChanged="gender_SelectedIndexChanged">
+               <asp:ListItem>男</asp:ListItem>
+               <asp:ListItem>女</asp:ListItem>
+           </asp:RadioButtonList>
+
+        <br />年齡:<asp:DropDownList ID="age" runat="server">
+               <asp:ListItem Value="0">未滿12歲</asp:ListItem>
+               <asp:ListItem Value="1">12~24</asp:ListItem>
+               <asp:ListItem Value="2">25~35</asp:ListItem>
+               <asp:ListItem Value="3">36~45</asp:ListItem>
+               <asp:ListItem Value="4">46~65</asp:ListItem>
+               <asp:ListItem Value="5">66歲以上</asp:ListItem>
+           </asp:DropDownList>
+           <br />
+             
+        身高:<asp:TextBox ID="heigh" runat="server" OnTextChanged="heigh_TextChanged"  ValidationGroup="12"></asp:TextBox>公分<br/>
+        體重:<asp:TextBox ID="weigh" runat="server" OnTextChanged="weigh_TextChanged"></asp:TextBox>公斤<br/>
+
+        血壓: 收縮壓:<asp:TextBox ID="Systolic" runat="server" OnTextChanged="Systolic_TextChanged"></asp:TextBox>mmHg 舒張壓:<asp:TextBox ID="Diastolic" runat="server" OnTextChanged="Diastolic_TextChanged"></asp:TextBox>mmHg<br/>
+
+        
+           每周運動次數:<asp:RadioButtonList ID="sport" runat="server"  AutoPostBack="true" RepeatDirection="Horizontal" RepeatLayout="Flow" OnSelectedIndexChanged="sport_SelectedIndexChanged1">
+               <asp:ListItem Value="0">0</asp:ListItem>
+               <asp:ListItem Value="1">1</asp:ListItem>
+               <asp:ListItem Value="2">2</asp:ListItem>
+               <asp:ListItem Value="3">3</asp:ListItem>
+               <asp:ListItem Value="4">&gt;3</asp:ListItem>
+           </asp:RadioButtonList>
+           <br/>
+
+        每次運動多久<asp:TextBox ID="sporttime" runat="server" OnTextChanged="sporttime_TextChanged"></asp:TextBox>公鐘<br/>
+
+           常做的運動:
+           <asp:CheckBoxList ID="ssport" runat="server" RepeatDirection="Horizontal" RepeatLayout="Flow" OnSelectedIndexChanged="ssport_SelectedIndexChanged">
+               <asp:ListItem Value="0">慢跑</asp:ListItem>
+               <asp:ListItem Value="1">長跑</asp:ListItem>
+               <asp:ListItem Value="2">游泳</asp:ListItem>
+               <asp:ListItem Value="3">瑜珈</asp:ListItem>
+               <asp:ListItem Value="4">騎腳踏車</asp:ListItem>
+               <asp:ListItem Value="5">籃球</asp:ListItem>
+               <asp:ListItem Value="6">羽球</asp:ListItem>
+               <asp:ListItem Value="7">跳舞</asp:ListItem>
+           </asp:CheckBoxList>
+           <br />
+
+          
+
+           每天喝多少杯含糖飲料:<asp:RadioButtonList ID="drink" runat="server" RepeatDirection="Horizontal" RepeatLayout="Flow">
+             <asp:ListItem Value="0">沒有喝</asp:ListItem>
+             <asp:ListItem Value="1">很少喝</asp:ListItem>
+             <asp:ListItem Value="2">1杯</asp:ListItem>
+             <asp:ListItem Value="3">2杯</asp:ListItem>
+             <asp:ListItem Value="4">>超過2杯</asp:ListItem>
+             
+           </asp:RadioButtonList><br/>
+
+            <asp:Button ID="Button1" runat="server" onclick="btnSumbit_Click" Text="確定" Width="100px" style="height: 27px" ValidationGroup="12" />
+            <br />
+           <asp:Label ID="Label8" runat="server" ForeColor="blue" Visible ="false"></asp:Label>
+               <br />
+           <asp:Panel ID="Panel1" runat="server" BackColor="#6699FF" BorderColor="#9900FF" Height="108px" Width="322px" Visible ="false" BorderStyle="Solid">
+               <asp:Label ID="Label1" runat="server" ForeColor="blue"></asp:Label>
+               <br />
+               <asp:Label ID="Label2" runat="server" ForeColor="blue"></asp:Label>
+               <br />
+               <asp:Label ID="Label3" runat="server" ForeColor="blue" ></asp:Label>
+               <br />
+                <asp:Label ID="Label7" runat="server" ForeColor="blue" ></asp:Label>
+               <asp:Label ID="Label6" runat="server" ForeColor="blue" ></asp:Label>
+               <asp:Label ID="Label4" runat="server" ForeColor="blue" ></asp:Label>
+               <br />
+               <asp:Label ID="Label5" runat="server" ForeColor="blue" ></asp:Label>
+              </asp:Panel>
+               <br />
+
+           <br />
+           <br />
+         </fieldset>
+     
+    </form>
+</body>
+</html>
---
+++ Default.aspx.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    double height = 0, weight = 0, bmi;
+    int stime, so = 0, su = 0;
+    int a, b, c, agerange;
+    
+    protected void Page_Load(object sender, EventArgs e)
+    { 
+        
+    }
+    protected void btnSumbit_Click(object sender, EventArgs e)
+    {
+
+        if(sport.SelectedIndex == 0 && sporttime.Text == "")
+        {sporttime.Text = "0";}
+
+       
+
+        if (name.Text == "" || heigh.Text == "" || weigh.Text == "" ||
+            Systolic.Text == "" || Diastolic.Text == "" || gender.SelectedValue == "" 
+            || sport.SelectedValue == "" || drink.SelectedValue == "")
+        {
+             
+             Response.Write("<script>alert('上述不能為空白')</script>"); 
+           
+            //ScriptManager.RegisterStartupScript(Page, GetType(),"alert","<script>alert('不可空白')</script>",false);
+
+        }
+        else
+        {
+            Label8.Visible = true;
+            Panel1.Visible = true;
+            Label8.Text = "您的健康狀況:";
+            if (ssport.SelectedIndex == -1)
+            {
+                Label7.Visible = false;
+                Label4.Visible = false;
+                Label6.Visible = false;
+                
+            }
+            else {
+                b = int.Parse(ssport.SelectedValue); //常做的運動
+                for (int i = 0; i < 5; i++)
+                {
+                    if (ssport.Items[i].Selected == true)
+                    {
+                        Label4.Text = "溫和運動!";
+                        Label4.Visible = true;
+                    }
+                }
+                for (int i = 5; i < 8; i++)
+                {
+                    if (ssport.Items[i].Selected == true)
+                    {
+                        Label6.Text = "激烈運動!";
+                        Label6.Visible = true;
+                    }
+                }
+                Label7.Text = "運動型態:";
+
+
+            }
+            
+
+            height = double.Parse(heigh.Text);
+            weight = double.Parse(weigh.Text);
+            so = int.Parse(Systolic.Text);
+            su = int.Parse(Diastolic.Text);
+            agerange = int.Parse(age.SelectedValue);
+            a = int.Parse(sport.SelectedValue); //每周運動次數
+            stime = int.Parse(sporttime.Text);//運動多久
+            //
+            c = int.Parse(drink.SelectedValue); //飲料次數
+            bmi = weight / ((height * height) / 10000);
+            //name.Text = Convert.ToString(bmi);
+
+            if (height < 0 || weight < 0) { Response.Write("<script>alert('身高體重不能為非負整數')</script>"); }
+            else {
+                if (bmi >= 24)
+                {
+                    Label1.Text = "bmi=" + bmi + "過重喔!";
+                }
+                else if (bmi < 18.5) { Label1.Text = "bmi=" + bmi + "過輕喔!"; }
+                else
+                {
+                    Label1.Text = "bmi=" + bmi;
+                }
+            
+            }
+
+           
+
+
+
+            if (agerange == 5) //範圍在66歲以上
+            {
+                if (so >= 130 && su >= 90 && so>su)
+                {
+                    Label2.Text = "年長者小心高血壓!";
+                }
+                else
+                {
+                    Label2.Text = " ";
+                }
+                if (a <= 2 || stime <= 30)
+                {
+                    Label3.Text = "年長者 太少運動了~";
+                }
+                else
+                {
+                    Label3.Text = " ";
+                }
+
+            }
+            else
+            {
+                if (so >= 120 && su >= 80 && so > su)
+                {
+                    Label2.Text = "小心高血壓!";
+                }
+                else
+                {
+                    Label2.Text = " ";
+                }
+                if (a <= 3 || stime <= 50)
+                {
+                    Label3.Text = "太少運動了~";
+                }
+                else
+                {
+                    Label3.Text = " ";
+                }
+            }
+
+
+            if (c >= 3)
+            {
+                Label5.Text = "喝太多含糖飲料囉!";
+            }
+            else
+            {
+                Label5.Text = "";
+            }
+            
+        }
+            
+        
+    }
+    protected void name_TextChanged(object sender, EventArgs e)  //name
+    {
+
+    }
+
+    protected void gender_SelectedIndexChanged(object sender, EventArgs e) //gender
+    {
+
+    }
+
+    protected void age_SelectedIndexChanged(object sender, EventArgs e) //age
+    {
+
+    }
+
+    protected void heigh_TextChanged(object sender, EventArgs e)  //heigh
+    {
+
+    }
+
+    protected void weigh_TextChanged(object sender, EventArgs e)  //weigh
+    {
+
+    }
+
+
+    protected void Systolic_TextChanged(object sender, EventArgs e)//收縮
+    {
+
+    }
+
+    protected void Diastolic_TextChanged(object sender, EventArgs e)//舒張
+    {
+
+    }
+
+    protected void sport_SelectedIndexChanged1(object sender, EventArgs e)
+    {
+        if (sport.SelectedIndex == 0)
+        {
+            sporttime.Enabled = false;
+        }
+        else {
+            sporttime.Enabled = true;
+        }
+    }
+    protected void sporttime_TextChanged(object sender, EventArgs e) //sport
+    {
+        
+    }
+    protected void ssport_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        
+    }
+}
